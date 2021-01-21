33a041133b71575ef6af69036a4734b4ac8a462e
fcracker79 <fcracker79@gmail.com>
2021-01-22 00:01:51 +0100

    Improved return type for region colors.

--- src/RegionColors.hs
+++ src/RegionColors.hs
@@ -1,10 +1,16 @@
 module RegionColors where
 
 import Text.HTML.Scalpel ( scrapeURL, attr, chroots, anySelector )
+import Control.Monad.Trans.Maybe(MaybeT)
+import Control.Monad.IO.Class(liftIO)
+import Control.Applicative(empty)
 
-
-getRegionColors :: IO (Maybe [(String, String)])
-getRegionColors = scrapeURL "https://covidzone.info" scraper
+getRegionColors :: MaybeT IO [(String, String)]
+getRegionColors = do
+    r <- liftIO $ scrapeURL "https://covidzone.info" scraper
+    case r of
+        Nothing -> return empty
+        Just x -> return x
     where scraper = chroots "path" $ do
                                      d <- attr "d" anySelector
                                      region  <- attr "title" anySelector
